91818b667ca4005e2d00b4980206ffacf0d118de
ranamagdi <ranagrida@gmail.com>
2023-05-02 00:39:58 +0300

    API

--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -1,18 +1,19 @@
 import { Component } from "react";
 import Navbar from "../HomePages/Navbar";
 import axios from "axios";
-import { Col, Container, Row } from "reactstrap";
+import { Col, Container, Row,Card,CardBody,CardTitle,CardText} from "reactstrap";
+
 
 class AboutPage extends Component{
     state={
         products:[]
     }
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://forkify-api.herokuapp.com/api/search?q=pizza')
         .then((result)=>{
             this.setState(
                 {
-                    products:result.data
+                    products:result.data.recipes
                 }
             )
         })
@@ -24,8 +25,20 @@ class AboutPage extends Component{
                <Container>
                 <Row>
                 {this.state.products.map((item)=>
-                <Col md="3">
-                 <h2>{item.title}</h2>
+                <Col md="4">
+                    <Card className="card_style">
+                        <img src={item.image_url}/>
+                        <CardBody>
+                            <CardTitle tag="h5">
+                           {item.title}
+                            </CardTitle>
+                        
+                            <CardText tag="h6">
+                            {item.publisher}
+                            </CardText>
+                         </CardBody>
+                        </Card>
+                 
                  </Col>
                )}
                 </Row>
